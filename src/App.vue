4f2635884fa8742101ede5207db91e23b5bec833
Dani Nebot <daniign@gmail.com>
2022-11-11 17:47:03 +0100

    Live coding session 5

--- src/App.vue
+++ src/App.vue
@@ -1,49 +1,123 @@
 <script setup>
-/*
-I have a new idea to download a huge database of movies and query it to find movies or to answer questions such as: what was the best action movie of the year 2015, or which year or years have the highest average score?
+import { ref, computed, watch } from 'vue';
+import MyButton from './components/MyButton.vue';
+import MOVIES from './assets/movies.json';
 
-Can you help me build it?
-
-Note: please do not update/change the file movies.json for this exercise!
-
-level 1: Let's have a list showing the movies in the movies.json file. Show all relevant information such as title, genre, score, etc. However, I'm collecting a lot of movies, so please paginate the list to show X movies at a time only, and add an input field where I can set this value of X. Of course, the list should update to the correct length at all times.
-
-level 2: Add first page, previous page, next page and last page buttons to the list. Make sure they are only active when relevant! Also, I would like to know the current page I'm looking at, and the total number of pages available.
-
-level 3: Add an input search field for the title of the movie. The list should update to show only movies with titles that match my search query. Please don't make it too restrictive, I would like for example that if I type 'wars', all 'Star Wars' movies should be matched.
-
-level 4: Add a multiselector for one or more years. The options should be all different years for which I have collected movies. Of course, the list should update to show only movies released in any of the chosen years.
-
-level 5: Add a multiselector for one or more genres. The options should be all different genres existing in the list of movies. Since I collected the movies from different sources, some genres are duplicated and don't really match, I expect you to fix this issue to show consistent options. Of course, the list should update to show only movies of any of the chosen genres.
-
-level 5: I'd like to know at all times what is the average score of ALL the movies that are listed (not considering the pagination), so please show it at the top of the list.
-
-bonus level: Apply nice styling to this UI!
-*/
-import { ref } from 'vue';
-import movies from './assets/movies.json';
-
+const movies = MOVIES.slice(); // clone input array
 const itemsPerPage = ref(5);
-const page = ref(6);
+const page = ref(1);
+const yearsSelected = ref([]);
+const filterTitle = ref('');
 
-// first element in the page: (page-1)* itemsPerPage
-// last element in the page: (page)* itemsPerPage
+function goToNext() {
+  page.value++;
+}
+
+function goToPrev() {
+  page.value--;
+}
+
+function filterByYear(movie) {
+  // (yearsSelected.value.length === 0) means no years were selected
+  if (yearsSelected.value.length === 0) return true;
+  return yearsSelected.value.includes(movie.year);
+}
+
+function filterByTitle(movie) {
+  if(filterTitle.value.length < 2) return true;
+  const lowerCaseTitle = filterTitle.value.toLowerCase();
+  return movie.title.toLowerCase().includes(lowerCaseTitle);
+}
+
+function avergageScore(list) {
+  const sum = list
+    .map(m => Number(m.score))
+    .reduce((a, b) => a + b, 0);
+  return (sum /list.length) || 0;
+}
+const filterMoviesAverageScore = computed(() => {
+  return avergageScore(filteredMovies.value);
+})
+
+const totalAverageScore = computed(() => {
+  return avergageScore(movies);
+})
+
+const currentPageMoviesAverageScore = computed(() => {
+  return avergageScore(moviesSnapshot.value);
+})
+const filteredMovies = computed(() => {
+  const fm = movies
+    .filter(filterByYear)
+    .filter(filterByTitle);
+
+    fm.sort((movie1, movie2) => {
+      return Number(movie2.score) - Number(movie1.score);
+    })
+    return fm;
+});
+
+const moviesSnapshot = computed(() => {
+    return filteredMovies.value
+    .slice(
+      (page.value - 1) * itemsPerPage.value,
+      page.value * itemsPerPage.value
+    );
+});
+
+const totalPages = computed(() => {
+  return Math.ceil(filteredMovies.value.length / itemsPerPage.value);
+});
+
+watch(itemsPerPage, () => {
+  page.value = 1;
+});
+
 </script>
 
 <template>
-  <input type="number" :value="itemsPerPage" />
+  <input type="number" v-model="itemsPerPage" /> <br/>
+  filter avergage score: {{ filterMoviesAverageScore }}<br/>
+  current page avg score: {{currentPageMoviesAverageScore}}<br/>
+  total score: {{ totalAverageScore }}<br/>
   <ul>
     <li
-      v-for="movie in movies.slice(
-        (page - 1) * itemsPerPage,
-        page * itemsPerPage
-      )"
+      v-for="movie in moviesSnapshot"
+      :key="movie.id"
     >
-      {{ movie.title }}
+      {{ movie.title }} || {{ movie.year }} || {{ movie.score }}
     </li>
   </ul>
-</template>
+  <h3>Pagination Controls</h3>
+  <MyButton
+    :disabled="page === 1"
+    @click="page = 1">
+    first page
+  </MyButton>
+  <MyButton
+    :disabled="page === 1"
+    @click="goToPrev">
+    &lt;
+  </MyButton>
+  {{ page }} / {{ totalPages }}
+  <MyButton
+  :disabled="page === totalPages"
+  @click="goToNext">
+    &gt;
+  </MyButton>
+  <MyButton
+    :disabled="page === totalPages"
+    @click="page = totalPages">
+    last page
+  </MyButton>
 
-<style scoped>
-/* TODO: */
-</style>
+  <hr />
+  <h3>Filters</h3>
+  <input v-model="filterTitle" type="text" />
+  <br/><br/>
+  <select v-model="yearsSelected" multiple>
+      <option>1999</option>
+      <option>2015</option>
+      <option>2022</option>
+    </select>
+</template>
